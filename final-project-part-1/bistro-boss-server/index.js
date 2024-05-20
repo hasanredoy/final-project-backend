0b37aa4b964c6ad84238bcb574c623ea66f35d0b
unknown <redoyhasan4585@gmail.com>
2024-05-20 22:02:17 +0400

    added day 4

--- final-project-part-1/bistro-boss-server/index.js
+++ final-project-part-1/bistro-boss-server/index.js
@@ -3,14 +3,17 @@ const cors = require('cors');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const cookie = require('cookie-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT ||5000
 
 const app =express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}))
 app.use(cookie())
 
 
@@ -30,15 +33,45 @@ async function run() {
   try {
     const menuCollection = client.db('menuDB').collection('menu')
     const reviewsCollection = client.db('menuDB').collection('reviews')
-
+    const cartsCollection = client.db('menuDB').collection('cart')
+// menu api 
  app.get('/menu', async(req,res)=>{
-  const getData = await menuCollection.find().toArray()
+  let query = {}
+  if(req.query.category){
+    query={category: req.query.category}
+  }
+  const size = parseInt(req?.query?.size)
+  const page = parseInt(req?.query?.page)
+  console.log(req.query.size,req.query.page);
+  const getData = await menuCollection.find(query).skip(size*page).limit(size).
+  toArray()
+ 
   res.send(getData)
  })
+// review api 
  app.get('/reviews', async(req,res)=>{
   const getData = await reviewsCollection.find().toArray()
   res.send(getData)
  })
+
+//  carts api 
+app.get('/cart',async(req,res)=>{
+  const userEmail = req.query.email
+  const query = {email: userEmail}
+  const result = await cartsCollection.find(query).toArray()
+  res.send(result)
+})
+app.post('/cart',async(req,res)=>{
+  const data = req.body
+  const result = await cartsCollection.insertOne(data)
+  res.send(result)
+})
+app.delete('/cart/:id',async(req,res)=>{
+  const id = req.params.id
+  const filter = {_id:new ObjectId(id)}
+  const result = await cartsCollection.deleteOne(filter)
+  res.send(result)
+})
   } finally {
   
   }
